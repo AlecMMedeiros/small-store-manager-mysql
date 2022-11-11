e682524df93ab18857f4bd40847830352a7f80a2
Alec Medeiros <alecmm@outlook.com>
2022-11-11 14:19:35 -0300

    Small tests adjusts

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# :construction: README em construção ! :construction:
+# Overview #
+
+This is a small project developed during my time as a full stack developer student.
+
+Project creates an API for dropshipping sales management in which you can create, view, delete and update products and sales. 
+
+Features in this app:
+
+* MSC (model-service-controller) architecture
+* Sinon and Chai-http
+* NodeJS
+* MySQL
+* ExpressJS
+* JavaScrip
+
+# Special thanks #
+
+Special thanks to all the Tryber staff for their support in my journey toward my career as a Full Stack developer.
--- tests/unit/controllers/products.controller.test.js
+++ tests/unit/controllers/products.controller.test.js
@@ -43,7 +43,7 @@ describe('Verificação da camada "Products controller" ', function () {
     expect(res.status).to.have.been.calledWith(200);
     expect(res.json).to.have.been.calledWith(listAllMock[0]);
   });
-  it('Verifica se ao cadastrar um produto, recebe-se o resumo do novo prodito e o código de resposta 201', async function () {
+  it('Verifica se ao cadastrar um produto, recebe-se o resumo do novo produto e o código de resposta 201', async function () {
     const res = {};
     const req = {
       body: {
@@ -64,11 +64,48 @@ describe('Verificação da camada "Products controller" ', function () {
       });
 
     await productsController.controllerInsertProduct(req, res);
-    expect(res.status).to.have.been.calledWith(201);   
+    expect(res.status).to.have.been.calledWith(201);
     expect(res.json).to.have.been.calledWith({ id: 4, name: "ProdutoX" });
 
   });
-  it('Verifica se ao atualizar um produto, recebe-se o resumo do novo prodito e o código de resposta 200', async function () {
+  it('Verifica se ao cadastrar um produto sem o nome, recebe-se o código de resposta 400', async function () {
+    const res = {};
+    const req = {
+      body: {
+      },
+      params: {
+        id: 1
+      },
+    };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    await productsController.controllerInsertProduct(req, res);
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.be.calledWith({ message: '"name" is required' });
+
+  });
+  it('Verifica se ao cadastrar um produto o tamanho do nome menor do que 5 caracters, recebe-se o código de resposta 422', async function () {
+    const res = {};
+    const req = {
+      body: {
+        name: "Min"
+      },
+      params: {
+        id: 1
+      },
+    };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    await productsController.controllerInsertProduct(req, res);
+    expect(res.status).to.have.been.calledWith(422);
+    expect(res.json).to.be.calledWith({ message: '"name" length must be at least 5 characters long' });
+
+  });
+  it('Verifica se ao atualizar um produto, recebe-se o resumo do novo produto e o código de resposta 200', async function () {
     const res = {};
     const req = {
       body: {
@@ -97,5 +134,96 @@ describe('Verificação da camada "Products controller" ', function () {
     expect(res.status).to.have.been.calledWith(200);
     expect(res.json).to.have.been.calledWith({ id: 1, name: "Martelo do Batman" });
 
+  });
+  it('Verifica se ao atualizar um produto, recebe-se o resumo do novo produto e o código de resposta 200', async function () {
+    const res = {};
+    const req = {
+      body: {
+        name: "Martelo do Batman"
+      },
+      params: {
+        id: 1
+      },
+    };
+
+    const response = {
+      code: 200,
+      message: {
+        id: 1,
+        name: "Martelo do Batman"
+      },
+    }
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(productsService, 'serviceUpdateProduct')
+      .resolves(response);
+
+    await productsController.controllerUpdateProduct(req, res);
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith({ id: 1, name: "Martelo do Batman" });
+
+  });
+  it('Verifica se ao atualizar um produto sem o nome, recebe-se o resumo do novo produto e o código de resposta 400', async function () {
+    const res = {};
+    const req = {
+      body: {
+      },
+      params: {
+        id: 1
+      },
+    };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    await productsController.controllerUpdateProduct(req, res);
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({ message: '"name" is required' });
+
+  });
+  it('Verifica se ao atualizar um produto com o tamanho do nome menor que 5 caracteres, recebe-se o resumo do novo produto e o código de resposta 422', async function () {
+    const res = {};
+    const req = {
+      body: {
+        name: "Min"
+      },
+      params: {
+        id: 1
+      },
+    };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    await productsController.controllerUpdateProduct(req, res);
+    expect(res.status).to.have.been.calledWith(422);
+    expect(res.json).to.have.been.calledWith({ message: '"name" length must be at least 5 characters long' });
+
+  });
+  it('Verifica se ao deletar um produto, recebe-se o código de resposta 204', async function () {
+    const res = {};
+    const req = {
+      params: {
+        id: 4
+      },
+    };
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    await productsController.controllerDeleteProduct(req, res);
+    expect(res.status).to.have.been.calledWith(204);
+  });
+  it('Verifica se ao buscar um produto usando um texto, recebe-se o código de resposta 200 e o resumo do produto', async function () {
+    const res = {};
+    const req = {
+      query: { q: "Martelo" }
+    };
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    await productsController.controllerSeachProduct(req, res);
+    expect(res.status).to.have.been.calledWith(200);
   });
 });
